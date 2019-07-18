352485ce69d20856b72ac4d02f70abf60a75202d
Addam Joor <38664958+MaddaRooj@users.noreply.github.com>
2019-07-18 16:42:24 -0500

    cleaned code and finished final problems

--- Classes/Cohort.cs
+++ Classes/Cohort.cs
@@ -6,11 +6,13 @@ namespace student_exercises
     public class Cohort
     {
         public string CohortName { get; set; }
-        public List<string> CohortStudents { get; set; }
-        public List<string> CohortInstructors { get; set; }
+        public List<Student> CohortStudents { get; set; }
+        public List<Instructor> CohortInstructors { get; set; }
         public Cohort(string cohort)
         {
             CohortName = cohort;
+            CohortStudents = new List<Student>();
+            CohortInstructors = new List<Instructor>();
         }
     }
 }
--- Program.cs
+++ Program.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Linq;
 
 namespace student_exercises
 {
@@ -8,38 +9,45 @@ namespace student_exercises
         static void Main(string[] args)
         {
             // Create exercises and add to list-----------------------------------
-            List<Exercise> exercises = new List<Exercise>();
             var exercise1 = new Exercise("C# Starter Project", "C#");
-            var exercise2 = new Exercise("React Nutshell", "ReactJS");
-            var exercise3 = new Exercise("english-idioms", "C#");
-            var exercise4 = new Exercise("urban-planner", "C#");
-            exercises.Add(exercise1);
-            exercises.Add(exercise2);
-            exercises.Add(exercise3);
-            exercises.Add(exercise4);
+            var exercise2 = new Exercise("React Nutshell", "Javascript");
+            var exercise3 = new Exercise("English Idioms", "C#");
+            var exercise4 = new Exercise("Urban Planner", "C#");
+            var exercise5 = new Exercise("Itinerary Builder", "Javascript");
+            var exercise6 = new Exercise("JS Nutshell", "Javascript");
+            List<Exercise> exercises = new List<Exercise>(){
+                exercise1, exercise2,exercise3, exercise4, exercise5, exercise6
+            };
 
             // Creates all cohorts------------------------------------------------
             var cohort1 = new Cohort("Day Classes - Cohort 32");
             var cohort2 = new Cohort("Day Classes - Cohort 33");
             var cohort3 = new Cohort("Day Classes - Cohort 34");
             var cohort4 = new Cohort("Night Classes - Cohort 16");
+            List<Cohort> cohorts = new List<Cohort>(){
+                cohort1, cohort2, cohort3, cohort4
+            };
 
             // Creates students and adds them to the list--------------------------
-            List<Student> students = new List<Student>();
-            var student1 = new Student("Addam", "Joor", "addam.joor", "Day Classes - Cohort 32");
-            var student2 = new Student("Emily", "Loggins", "emily.loggins", "Day Classes - Cohort 32");
-            var student3 = new Student("Ellie", "Ash", "ellie.ash", "Day Classes - Cohort 34");
-            var student4 = new Student("Ben", "Parker", "ben.parker", "Day Classes - Cohort 33");
-            students.Add(student1);
-            students.Add(student2);
-            students.Add(student3);
-            students.Add(student4);
+            var student1 = new Student("Addam", "Joor", "addam.joor", cohort1);
+            var student2 = new Student("Emily", "Loggins", "emily.loggins", cohort1);
+            var student3 = new Student("Ellie", "Ash", "ellie.ash", cohort3);
+            var student4 = new Student("Ben", "Parker", "ben.parker", cohort2);
+            var student5 = new Student("Shelley", "Arnold", "shelley.arnold", cohort1);
+            List<Student> students = new List<Student>(){
+                student1, student2, student3, student4, student5
+            };
+            cohort1.CohortStudents.Add(student1);
+            cohort1.CohortStudents.Add(student2);
+            cohort1.CohortStudents.Add(student5);
+            cohort3.CohortStudents.Add(student3);
+            cohort2.CohortStudents.Add(student4);
 
             // Creates all instructors--------------------------------------------
-            var instructor1 = new Instructor("Dance", "Storm", "dan.storm", "Day Classes - Cohort 32", "Dancesplosion");
-            var instructor2 = new Instructor("Bryan", "Nielson", "bryanfuckingnielson", "Day Classes - Cohort 32", "High Five Hurricane");
-            var instructor3 = new Instructor("Brenda", "Long", "brenda.long", "Day Classes - Cohort 34", "Positivity Overload");
-            var instructor4 = new Instructor("Adam", "Sheaffer", "adam.sheaffer", "Day Classes - Cohort 33", "Hat Heard Round the World");
+            var instructor1 = new Instructor("Dance", "Storm", "dan.storm", cohort1, "Dancesplosion");
+            var instructor2 = new Instructor("Bryan", "Nielson", "bryanfuckingnielson", cohort1, "High Five Hurricane");
+            var instructor3 = new Instructor("Brenda", "Long", "brenda.long", cohort3, "Positivity Overload");
+            var instructor4 = new Instructor("Adam", "Sheaffer", "adam.sheaffer", cohort2, "Hat Heard Round the World");
 
             foreach(Student student in students)
             {
@@ -52,6 +60,7 @@ namespace student_exercises
                 Console.WriteLine($"Assignment: {exercise.ExerciseName} in {exercise.ExerciseLanguage}");
             };
 
+            // Instructor assigns student with a specified exercise
             System.Console.WriteLine();
             instructor4.Assign(student1, exercise2);
             instructor2.Assign(student1, exercise3);
@@ -60,6 +69,7 @@ namespace student_exercises
             instructor4.Assign(student4, exercise2);
             System.Console.WriteLine();
 
+            // Consoles the student name followed by their respective list of assignments
             foreach(var student in students){
                 System.Console.WriteLine($"{student.FirstName}'s Assignment List:");
                 foreach(var exercise in student.StudentExercises){
@@ -67,6 +77,69 @@ namespace student_exercises
                 }
                 System.Console.WriteLine();
             }
+
+            // Select all exercises that utilize Javascript language
+            var javaExercises = from exercise in exercises
+                where exercise.ExerciseLanguage == "Javascript"
+                select exercise;
+
+            System.Console.WriteLine("All Javascript exercises:");
+            foreach(Exercise exercise in javaExercises){
+                System.Console.WriteLine(exercise.ExerciseName);
+            }
+
+            // Select all students that are in cohort 32
+            System.Console.WriteLine();
+            var studentsOfCohort32 = from student in students
+                where student.StudentCohort == cohort1
+                select student;
+
+            System.Console.WriteLine("All Students in Cohort 32:");
+            foreach (Student student in studentsOfCohort32){
+                System.Console.WriteLine($"{student.FirstName} {student.LastName}");
+            }
+
+            // Order all students by last name
+            System.Console.WriteLine();
+            var sortedStudents = from student in students
+                orderby student.LastName
+                select student;
+            System.Console.WriteLine("All students ordered by last name:");
+            foreach(Student student in sortedStudents){
+                System.Console.WriteLine($"{student.FirstName} {student.LastName}");
+            }
+
+            // Sort students with no exercises assigned
+            System.Console.WriteLine();
+            var noExercises = from student in students
+                where student.StudentExercises.Count == 0
+                select student;
+            System.Console.WriteLine("Students with no exercises assigned:");
+            foreach(Student student in noExercises){
+                System.Console.WriteLine($"{student.FirstName} {student.LastName}");
+            }
+
+            // Find student with the most exercises assigned
+            System.Console.WriteLine();
+            var mostExercises = from student in students
+                orderby student.StudentExercises.Count descending
+                select student;
+            System.Console.WriteLine("Student with most exercises:");
+            foreach(Student student in mostExercises){
+                System.Console.WriteLine($"{student.FirstName} {student.LastName} - {student.StudentExercises.Count}");
+            };
+
+            // How many students per cohort?
+            System.Console.WriteLine();
+            var studentCount = from cohort in cohorts
+                orderby cohort.CohortStudents.Count descending
+                select cohort;
+            System.Console.WriteLine("Number of students per cohort:");
+            foreach(Cohort cohort in studentCount){
+                System.Console.WriteLine($"{cohort.CohortName} - {cohort.CohortStudents.Count}");
+            };
+
+
         }
     }
 }
